b28fbce738d67d847c3d894935b8340e455c1156
Chetan Kini <ckini123@gmail.com>
2021-01-09 19:57:42 -0500

    feat(visuals): Create template to store content
    Go uses the html/template libary to provide support for HTML templating.
    Go structs can be called upon using {{}}; we are to pass the root
    utils.Directory object and call its ToJSON method to populate our chart
    with the necessary JSON data.

--- src/visuals/templates.go
+++ src/visuals/templates.go
@@ -1,12 +1,36 @@
 package visuals
 
-func temp() string {
-	return `	
-	<html>
-	  <head>
-	  </head>
-	  <body>
-	  </body>
-	</html>
-	`
+import (
+	"github.com/cdkini/clocviz/src/utils"
+	"html/template"
+	"log"
+)
+
+func generateTemplate(root *utils.Directory) {
+	template, err := template.ParseFiles("out.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err := t.Execute(os.Stdout, root)
+
 }
+
+// func openbrowser(url string) {
+// 	var err error
+
+// 	switch runtime.GOOS {
+// 	case "linux":
+// 		err = exec.Command("xdg-open", url).Start()
+// 	case "windows":
+// 		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+// 	case "darwin":
+// 		err = exec.Command("open", url).Start()
+// 	default:
+// 		err = fmt.Errorf("unsupported platform")
+// 	}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// }
