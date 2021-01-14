387bf573c3f81b082f7b613c56f660dfb8b0541d
Chetan Kini <ckini123@gmail.com>
2021-01-14 18:19:26 -0500

    feat(web): Populate stats in template
    Added JS to update the stats card of the index.tmpl HTML file used in
    output. See script.js for helper methods.

--- src/web/assets/script.js
+++ src/web/assets/script.js
@@ -21,3 +21,26 @@ data.children.forEach((child) =>
   }
   `)
 );
+
+function wrapper(id, node) {
+  clear(id);
+  populateStats(node);
+}
+
+function clear(id) {
+  document.getElementById(id).innerHTML = "";
+}
+
+function populateStats(node) {
+  if (!node.hasOwnProperty("children")) {
+    document.getElementById(
+      "statsList"
+    ).innerHTML += `<li>${node.name}: ${node.size}</li>`;
+    console.log(node.name, node.size);
+  } else {
+    console.log("Folder", node.name);
+    node.children.map(populateStats);
+  }
+}
+
+wrapper("statsList", data);
